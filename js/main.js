f6c0b48effefcd9ac64870099146fd7837e1f1b6
Robby <robby.azucena@gmail.com>
2021-09-29 22:53:41 +0800

    Update css and added images

---
+++ js/main.js
@@ -0,0 +1,19 @@
+function artstationIMG(){
+    var element = document.getElementById("banner-artstation");
+    element.classList.add("active");
+}
+
+function defaultIMG_artstation(){
+    var element = document.getElementById("banner-artstation");
+    element.classList.remove("active");
+}
+
+function behanceIMG(){
+    var element = document.getElementById("banner-behance");
+    element.classList.add("active");
+}
+
+function defaultIMG_behance(){
+    var element = document.getElementById("banner-behance");
+    element.classList.remove("active");
+}
