a7959224962c1ef702ea7f2d8ac20c7c6b6bd5b7
RT_Robby <rt_robby@RT-Robbys-MacBook-Pro.local>
2021-09-30 16:20:22 +0800

    Update loading screen

--- js/main.js
+++ js/main.js
@@ -18,10 +18,34 @@ function defaultIMG_behance(){
     element.classList.remove("active");
 }
 
+
+
+
+var elem = document.getElementById("myBar");
+var percent = document.getElementById("percent");
+var width = 10;
+var id = setInterval(frame, Math.random() * (500 - 100) + 100);
+function frame() {
+    if (width >= 100) {
+      clearInterval(id);
+      i = 0;
+    } else {
+      width++;
+      elem.style.width = width + "%";
+      percent.innerHTML = width + "%";
+    }
+}
+
 window.onload = function(){
-    document.getElementById("loading").style.opacity='0';
+    id = setInterval(frame, 10)
     setTimeout(function(){ 
+        document.getElementById("loading").style.opacity='0';
+    }, 1000);
+    
+    
+    setTimeout(function(){ 
+        console.log('Loading screen gone!')
         document.getElementById("loading").style.zIndex='-10000';
-    }, 500);
+    }, 1300);
     
 }
